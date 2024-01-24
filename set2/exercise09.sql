141e5f6036ac3b6ebe53d73e732123ddde73f405
Konstantinos Lampropoulos <konstantinoslamprop@gmail.com>
2024-01-25 00:31:45 +0200

    Delete set2 directory

--- set2/exercise09.sql
+++
@@ -1,9 +0,0 @@
-SELECT name AS Country, IndepYear AS 'Independence Year', 
-       LifeExpectancy AS 'Life Expectancy', Continent,
-	   Population, SurfaceArea AS Surface
-FROM country
-WHERE IndepYear IS NULL
-  AND Continent IN ('Africa', 'Asia')
-  AND Population >= 100000
-  AND (SurfaceArea < 10000 OR SurfaceArea > 100000)
-ORDER BY Population DESC;
